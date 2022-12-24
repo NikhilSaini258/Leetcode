a62daec2507c5f09c88d178c11e5e451155adf2a
Nikhil Saini <nikhilsaini258@gmail.com>
2022-12-24 14:31:19 +0530

    Update 122. Best Time to Buy and Sell Stock II.cpp

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -51,6 +51,55 @@ public:
     }
 };
 
+// DP with tabulation
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // buy --> 0 --> Sell
+        // buy --> 1 --> Buy
+        int n = prices.size();
+        vector<vector<int>> dp (n + 1, vector<int>(2, -1));
+        dp[n][0] = dp[n][1] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 1; buy >= 0; buy--) {
+                if (buy == 1) {     // Buy stock condition
+                    dp[i][buy] = std::max(-prices[i] + dp[i+1][0],
+                                        0 + dp[i+1][1] );
+                } else {            // Sell stock condition
+                    dp[i][buy] = std::max(prices[i] + dp[i+1][1],
+                                        0 + dp[i+1][0] );
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
+
+// DP with tabulation with space optimized
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // buy --> 0 --> Sell
+        // buy --> 1 --> Buy
+        int n = prices.size();
+        vector<int> ahead (2, 0), curr (2,0);
+        ahead[0] = ahead[1] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 1; buy >= 0; buy--) {
+                if (buy == 1) {     // Buy stock condition
+                    curr[buy] = std::max(-prices[i] + ahead[0],
+                                        0 + ahead[1] );
+                } else {            // Sell stock condition
+                    curr[buy] = std::max(prices[i] + ahead[1],
+                                        0 + ahead[0] );
+                }
+                ahead = curr;
+            }
+        }
+        return ahead[1];
+    }
+};
+
 // Cheat solution
 class Solution {
 public:
