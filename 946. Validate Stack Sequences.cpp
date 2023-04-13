8bca7f9f32abcdb01a2c2601523f9baf9c150cf5
Nikhil Saini <nikhilsaini258@gmail.com>
2023-04-13 13:29:08 +0530

    Update 946. Validate Stack Sequences.cpp

--- 946. Validate Stack Sequences.cpp
+++ 946. Validate Stack Sequences.cpp
@@ -13,3 +13,20 @@ public:
         return s.empty();
     }
 };
+
+// Space Optimized solution
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int i = 0;
+        int j = 0;
+        for (auto& e: pushed) {
+            pushed[i++] = e;
+            while (i > 0 && popped[j] == pushed[i-1]) {
+                i--;;
+                j++;
+            }
+        }
+        return i == 0;
+    }
+};
