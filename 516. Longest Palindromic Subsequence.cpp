f72bf79005355eafa0142f8ae774e54fdde6db39
Nikhil Saini <nikhilsaini258@gmail.com>
2023-04-18 00:44:46 +0530

    Update 516. Longest Palindromic Subsequence.cpp

--- 516. Longest Palindromic Subsequence.cpp
+++ 516. Longest Palindromic Subsequence.cpp
@@ -1,3 +1,50 @@
+// Recursive approach
+class Solution {
+public:
+    int solve (string& s1, string& s2, int i, int j) {
+        if (i == 0 || j == 0) {
+            return 0;
+        }
+        if (s1[i-1] == s2[j-1]) {
+            return 1 + solve (s1, s2, i-1, j-1);
+        } else {
+            return std::max (solve (s1, s2, i-1, j), solve (s1, s2, i, j-1));
+        }
+    }
+    int longestPalindromeSubseq(string s) {
+        string s2 = s;
+        std::reverse (s2.begin(), s2.end());
+        return solve (s, s2, s.size(), s2.size());
+    }
+};
+
+// Memorization approach
+class Solution {
+public:
+    int solve (string& s1, string& s2, int i, int j, vector<vector<int>>& dp) {
+        if (i == 0 || j == 0) {
+            dp[i][j] = 0;
+            return dp[i][j];
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (s1[i-1] == s2[j-1]) {
+            dp[i][j] = 1 + solve(s1, s2, i-1, j-1, dp);
+        } else {
+            dp[i][j] = std::max (solve(s1, s2, i-1, j, dp), solve(s1, s2, i, j-1, dp));
+        }
+        return dp[i][j];
+    }
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>> dp (s.size()+1, vector<int>(s.size()+1, -1));
+        string s2 = s;
+        std::reverse (s2.begin(), s2.end());
+        return solve (s, s2, s.size(), s2.size(), dp);
+    }
+};
+
+// Top Down Approach
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
