96c85d00d01725068adeb94a364fbb8d3ceeb69d
Nikhil Saini <nikhilsaini258@gmail.com>
2023-05-04 12:13:48 +0530

    Update 138. Copy List with Random Pointer.cpp

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -37,3 +37,60 @@ public:
         return um[head];
     }
 };
+
+// Optimized
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) {
+            return head;
+        }
+
+        Node* iter = head;
+        Node* nxt;
+
+        // Creating new nodes and handling next pointer
+        // Also merging 2 LL, side by side: oldNode1->newNode1->oldNode2->newNode2->oldNode3->newNode3
+        while (iter != nullptr) {
+            nxt = iter->next;
+
+            Node* newNode = new Node (iter->val);
+            newNode->next = nxt;
+            iter ->next = newNode;
+
+            iter = nxt;
+        }
+        iter = head;
+
+        // Handling random pointer
+        while (iter != nullptr) {
+            if (iter->random != nullptr) {
+                iter->next->random = iter->random->next;
+            }
+            iter = iter->next->next;
+        }
+        
+        // Restore the original list, and extract the copy list
+        iter = head;
+        Node* copy;
+        Node* newHead = new Node(0);
+        newHead->next = iter->next;
+
+        while (iter != nullptr) {
+            nxt = iter->next->next;
+            copy = iter->next;
+
+            iter->next = nxt;
+            if (nxt != nullptr) {
+                copy->next = nxt->next;
+            } else {
+                copy->next = nullptr;
+            }
+            // restore the original list
+            iter->next = nxt;
+            
+            iter = nxt;
+        }
+        return newHead->next;
+    }
+};
